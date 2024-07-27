2faa12b3740c3d3d4c76b94f9a69d34abb0f45a8
JiMo20 <Jmouhtasseb@gmail.com>
2024-07-27 09:51:23 +0400

    modified college to school

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ let users = [
   {
     name: "Ahmed",
     age: 29,
+    college: "Caltech",
   },
   {
     name: "Ali",
     age: 30,
+    college: "SANS",
+  },
+  {
+    name: "Miles",
+    age: 30,
+    college: "TMC",
   },
 ];
 
+// let updatedUser = users.map((user, index) => {
+//   if (index == 0) {
+//     return { ...user, age: user.age + 7, college: "School" };
+//     //user.age is the value of the key, age
+//   } else {
+//     return user;
+//   }
+// });
+// console.log(updatedUser, "new array");
+// console.log(users.length, "new array");
+
 let updatedUser = users.map((user, index) => {
-  if (index == 0) {
-    return { ...user, age: user.age + 7 };
-    //user.age is the value of the key, age
-  } else {
-    return user;
-  }
+  // delete user.collage;
+  return { ...user, college: "school" };
 });
 console.log(updatedUser, "new array");
 console.log(users.length, "new array");
