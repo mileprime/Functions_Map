1dea1831fe6ffcd5026ba70bf9615cad0e7a998c
JiMo20 <Jmouhtasseb@gmail.com>
2024-07-27 10:24:32 +0400

    deleting a user using filter

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 let users = [
   {
+    id: 44,
     name: "Ahmed",
     age: 29,
     college: "Caltech",
   },
   {
+    id: 23,
     name: "Ali",
     age: 30,
     college: "SANS",
   },
   {
+    id: 25,
     name: "Miles",
     age: 30,
     college: "TMC",
   },
 ];
 
-//create new array for forEach method
-//use push instead of return for forEach Method
+/**
+ * Delete using filter
+ * User id not equal to id(to be deleted)
+ */
 
-let updatedUser = [];
-users.forEach((user) => {
-  if (user.name == "Ali") {
-    updatedUser.push({ ...user, age: 9 });
-  } else updatedUser.push(user);
+let deleetingid = 44;
+let updatedUsers = users.filter((user) => {
+  return user.id != deleetingid;
 });
-console.log(updatedUser);
+console.log(updatedUsers);
+
+/**
+ * create new array for forEach method
+use push instead of return for forEach Method
+
+ */
+
+// let updatedUser = [];
+// users.forEach((user) => {
+//   if (user.name == "Ali") {
+//     updatedUser.push({ ...user, age: 9 });
+//   } else updatedUser.push(user);
+// });
+// console.log(updatedUser);
 
 // let updatedUser = users.map((user, index) => {
 //   if (index == 0) {
